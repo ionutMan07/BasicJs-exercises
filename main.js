a51bc8c98304cf7e73bf773d3e2af0118d869be9
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-04 22:26:55 +0300

    new

---
+++ main.js
@@ -0,0 +1,11 @@
+// alert("I'm a good script");
+// checking number is odd or
+
+let number = 3;
+if (number % 2 === 0) {
+  console.log("Number is even");
+} else {
+  console.log("Number is odd");
+}
+
+//
